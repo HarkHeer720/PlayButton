181f99ce8ee265833bcb49525a9322de0448d8a0
harkheer720 <harkheer720@SDS-L-F93HC63>
2023-02-09 14:02:08 -0500

    Add project files.

---
+++ PlayButton/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace PlayButton
+{
+    partial class Background
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Background));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(119, 110);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(323, 82);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Press to start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Background
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(556, 303);
+            this.Controls.Add(this.StartButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Background";
+            this.Text = "Play Button";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ PlayButton/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayButton
+{
+    public partial class Background : Form
+    {
+        public Background()
+        {
+            InitializeComponent();
+        }
+        
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer newMario = new SoundPlayer(Properties.Resources.newMario);
+            newMario.Play();
+
+            Thread.Sleep(500);
+            
+            StartButton.Text = "Game will start in 3";
+
+            Refresh();
+            Thread.Sleep(1000);
+             
+
+            StartButton.Text = "Game will start in 2";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            StartButton.Text = "Game will start in 1";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            StartButton.Text = "Go";
+            StartButton.BackColor = Color.Green;
+            BackColor = Color.Green;
+
+            Refresh();
+            Thread.Sleep(5000);
+            BackColor = Color.Black;
+            StartButton.Text = "Press to start";
+            StartButton.BackColor = Color.White;
+        }
+    }
+}
